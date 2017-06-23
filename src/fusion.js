703d28015ec012625a431506135af5ecb4b32116
Sarun Rattanasiri <midnight_w@gmx.tw>
2017-06-24 00:41:25 +0700

    rewrite redux-fusion to leverage on react-redux

--- src/fusion.js
+++ src/fusion.js
@@ -1,39 +1,31 @@
-import React, { createElement } from 'react'
-import { mapPropsStream } from 'recompose'
-import { Observable } from 'rxjs'
+import * as React from 'react'
+import { connect } from 'react-redux'
+import { default as compose } from 'recompose/compose'
+import { default as mapProps } from 'recompose/mapProps'
+import { mapPropsStreamWithConfig } from 'recompose/mapPropsStream'
+import { default as pure } from 'recompose/pure'
+import { default as rxjsConfig } from 'recompose/rxjsObservableConfig'
+import { ReplaySubject } from 'rxjs/ReplaySubject'
+import 'rxjs/add/operator/distinctUntilChanged'
+import 'rxjs/add/operator/publishReplay'
 
-export default propStream$ => (StreamedComponent) => {
-  class ComponentFromStream extends React.Component {
-    constructor(props, context) {
-      super(props, context);
-      const { store } = context;
-      if (!store) {
-        throw new Error(
-          `fuse() error: Missing Redux store in context.
-           Did you forget to <Provide> it?`
-        )
-      }
-      this.WrappedComponent = mapPropsStream(
-        propStream$(Observable.from(store), store.dispatch)
-      )(StreamedComponent)
-    }
+const mapPropsStream = mapPropsStreamWithConfig(rxjsConfig)
+const stateProxy = new ReplaySubject(1)
+const nullifiedState = {}
+const stateSpy = connect((storeState) => {
+  stateProxy.next(storeState)
+  return nullifiedState
+});
+const state$ = stateProxy.distinctUntilChanged().publishReplay(1).refCount()
+const dispatchSpy = compose(
+  stateSpy,
+  mapProps(({dispatch}) => dispatch),
+  pure,
+)
+const fuse = (handler) => (Component) => dispatchSpy((dispatch) => {
+  const enhance = mapPropsStream(handler(state$, dispatch))
+  const EnhancedComponent = enhance(Component)
+  return <EnhancedComponent/>
+});
 
-    render() {
-      return createElement(this.WrappedComponent)
-    }
-  }
-  ComponentFromStream.contextTypes = {
-    store: (props, propName) => {
-      // custom validation via Observable.from
-      try {
-        Observable.from(props[propName])
-      } catch (e) {
-        return
-          `fuse() error: context.store must implement observable` // eslint-disable-line
-      }
-      return null
-    }
-  }
-
-  return ComponentFromStream
-}
+export default fuse
