0856ddf907e9319e4498d63092b055d15e26294e
Sarun Rattanasiri <midnight_w@gmx.tw>
2017-06-25 22:41:41 +0700

    rework the implementation

--- src/fusion.js
+++ src/fusion.js
@@ -1,31 +1,66 @@
 import * as React from 'react'
 import { connect } from 'react-redux'
-import { default as compose } from 'recompose/compose'
-import { default as mapProps } from 'recompose/mapProps'
-import { mapPropsStreamWithConfig } from 'recompose/mapPropsStream'
-import { default as pure } from 'recompose/pure'
+import { componentFromStreamWithConfig } from 'recompose/componentFromStream'
 import { default as rxjsConfig } from 'recompose/rxjsObservableConfig'
+import { default as shallowEqual } from 'recompose/shallowEqual'
+import { BehaviorSubject } from 'rxjs/BehaviorSubject'
 import { ReplaySubject } from 'rxjs/ReplaySubject'
 import 'rxjs/add/operator/distinctUntilChanged'
+import 'rxjs/add/operator/let'
+import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/publishReplay'
 
-const mapPropsStream = mapPropsStreamWithConfig(rxjsConfig)
+const namespece = 'reduxFusion'
+const componentFromStream = componentFromStreamWithConfig(rxjsConfig)
 const stateProxy = new ReplaySubject(1)
 const nullifiedState = {}
-const stateSpy = connect((storeState) => {
+const stateSpy = (storeState) => {
   stateProxy.next(storeState)
   return nullifiedState
-});
+}
+const dispatchKey = `${namespece}Dispatch`
+const dispatchSpy = (dispatch) => ({ [dispatchKey]: dispatch })
+const connectSpy = connect(stateSpy, dispatchSpy)
 const state$ = stateProxy.distinctUntilChanged().publishReplay(1).refCount()
-const dispatchSpy = compose(
-  stateSpy,
-  mapProps(({dispatch}) => dispatch),
-  pure,
-)
-const fuse = (handler) => (Component) => dispatchSpy((dispatch) => {
-  const enhance = mapPropsStream(handler(state$, dispatch))
-  const EnhancedComponent = enhance(Component)
-  return <EnhancedComponent/>
-});
+
+const fuse = (connectHandler) => (Component) => {
+  class FusedComponent extends React.Component {
+    constructor(props) {
+      super(props)
+      const { [dispatchKey]: dispatch, ...pureProps } = props
+      const propsProxy = new BehaviorSubject(pureProps)
+      const propsHandler = connectHandler(state$, dispatch)
+      const transformedProps$ = propsProxy
+        .distinctUntilChanged(shallowEqual)
+        .let(propsHandler)
+        .distinctUntilChanged(shallowEqual)
+      const applyPropsStream = (transformedProps) => <Component {...transformedProps}/>
+      this.propsProxy = propsProxy
+      this.ConnectedComponent = componentFromStream(
+        () => transformedProps$.map(applyPropsStream),
+      )
+    }
+
+    componentWillReceiveProps(nextProps) {
+      // eslint-disable-next-line no-unused-vars
+      let { [dispatchKey]: unused, ...pureProps } = nextProps;
+      this.propsProxy.next(pureProps)
+    }
+
+    shouldComponentUpdate() {
+      return false
+    }
+
+    propsProxy
+    ConnectedComponent
+
+    render() {
+      const { ConnectedComponent } = this
+      return <ConnectedComponent/>
+    }
+  }
+
+  return connectSpy(FusedComponent)
+}
 
 export default fuse
